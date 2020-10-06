00e9267fffd91fb8d633f4298b4400f08de78bf0
Tejasvi Vig <tejasvivig09@gmail.com>
2020-10-06 19:31:19 -0300

    Changing the hello world class to accommodate the main method

--- src/main/java/asdc/hl/HelloWorld.java
+++ src/main/java/asdc/hl/HelloWorld.java
@@ -1,8 +1,11 @@
 package asdc.hl;
 
 public class HelloWorld {
+    public static void main(String args[]) {
+        System.out.println("Welcome to the main class");
+    }
     public String Hello (String str){
-        System.out.println("Hello "+str);
+        System.out.println("Hello " + str);
         return str;
     }
 }
