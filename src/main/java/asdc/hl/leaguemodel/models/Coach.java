86948a2fb08b8326643160588f5fbc35016eb3f1
Rajveen <rj660238@dal.ca>
2020-10-09 21:48:37 -0300

    Coach object

--- src/main/java/asdc/hl/leaguemodel/models/Coach.java
+++ src/main/java/asdc/hl/leaguemodel/models/Coach.java
@@ -1,4 +1,76 @@
 package asdc.hl.leaguemodel.models;
 
-public class Coach {
+import asdc.hl.leaguemodel.IPersistence;
+
+public class Coach implements ICoach{
+
+    private int coachID;
+    private String coachName;
+    private int teamID;
+
+    private IPersistence db;
+
+    public Coach(IPersistence p, int teamId) {
+        this();
+        teamID = teamId;
+        db = p;
+    }
+
+    public Coach() {
+        coachID = -1;
+        coachName = null;
+    }
+
+    @Override
+    public int getCoachID() {
+        return coachID;
+    }
+
+    @Override
+    public void setCoachID(int id) {
+        coachID = id;
+    }
+
+    @Override
+    public String getCoachName() {
+        return coachName;
+    }
+
+    @Override
+    public void setCoachName(String name) {
+        coachName = name;
+    }
+
+    @Override
+    public int getTeamID() {
+        return teamID;
+    }
+
+    @Override
+    public void setTeamID(int id) {
+        teamID = id;
+    }
+
+    private boolean isNullOrEmpty(String s) {
+        return s == null || s.equals("");
+    }
+
+    @Override
+    public String validateName() {
+        String validity = "Valid";
+        if (isNullOrEmpty(coachName)) {
+            validity = "Coach name is blank";
+        }
+        return validity;
+    }
+
+    @Override
+    public void save() {
+        db.saveCoach(this);
+    }
+
+    @Override
+    public void load() {
+        db.loadCoachWithName(coachName, this);
+    }
 }
