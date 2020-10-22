8f04313f2bd8f02bdc70e508cb38dca4050f443c
Tejasvi Vig <tejasvivig09@gmail.com>
2020-10-22 19:00:23 -0300

    Adding the old code again

---
+++ src/main/java/com/leaguemodel/models/IFreeAgent.java
@@ -0,0 +1,25 @@
+package com.leaguemodel.models;
+
+public interface IFreeAgent {
+    int getPlayerID();
+    void setPlayerID(int id);
+
+    String getPlayerName();
+    void setPlayerName(String name);
+
+    boolean getIsCaptain();
+    void setIsCaptain(boolean isCaptain);
+
+    String getPlayerPosition();
+    void setPlayerPosition(String name);
+
+
+    String validateNameDuringImport();
+    String validateNameDuringCreate();
+    String validateNameDuringLoad();
+
+    String validateBusinessRules();
+
+//    void save();
+//    void load();
+}
---
+++ src/main/java/com/leaguemodel/models/ILeague.java
@@ -0,0 +1,29 @@
+package com.leaguemodel.models;
+
+import java.util.List;
+
+public interface ILeague {
+    String getLeagueName();
+    void setLeagueName(String name);
+
+    int getLeagueID();
+    void setLeagueID(int id);
+
+    IConference getConferenceByName(String name);
+    void addConference(IConference conference);
+
+    List<IConference> getConferences();
+    void setConferences(List<IConference> conferences);
+
+    List<IPlayer> getFreeAgents();
+    void setFreeAgents(List<IPlayer> freeAgents);
+
+    String validateNameDuringImport();
+    String validateNameDuringLoad();
+    String validateNameDuringCreate();
+
+    String validateBusinessRules();
+
+    void save();
+    void load();
+}
