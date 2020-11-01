378967afeeb8cba6b75e336c98c1980cd1a0a306
Tejasvi Vig <tejasvivig09@gmail.com>
2020-11-01 19:02:56 -0400

    Implement training

--- src/main/java/com/IceHockeyLeague/LeagueManager/Team/ITeamTraining.java
+++ src/main/java/com/IceHockeyLeague/LeagueManager/Team/ITeamTraining.java
@@ -1,5 +1,10 @@
 package com.IceHockeyLeague.LeagueManager.Team;
 
-public interface ITeamTraining {
+import com.IceHockeyLeague.LeagueManager.Coach.ICoachStats;
+import com.IceHockeyLeague.LeagueManager.Player.ITeamPlayer;
 
+import java.util.List;
+
+public interface ITeamTraining {
+    void trainTeam(List<ITeamPlayer> teamPlayers, ICoachStats coachStats);
 }
