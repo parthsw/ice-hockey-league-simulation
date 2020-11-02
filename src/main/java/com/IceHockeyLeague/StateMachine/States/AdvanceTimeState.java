0163df8ef7ca3cf4bb31ffa33a4668b13449134d
Rajveen Singh Chandhok <rj660238@dal.ca>
2020-11-02 15:04:58 -0400

    Code changes to states

--- src/main/java/com/IceHockeyLeague/StateMachine/States/AdvanceTimeState.java
+++ src/main/java/com/IceHockeyLeague/StateMachine/States/AdvanceTimeState.java
@@ -1,6 +1,12 @@
 package com.IceHockeyLeague.StateMachine.States;
 
+import com.IceHockeyLeague.LeagueManager.AbstractLeagueManagerFactory;
+import com.IceHockeyLeague.LeagueManager.Conference.IConference;
+import com.IceHockeyLeague.LeagueManager.Division.IDivision;
 import com.IceHockeyLeague.LeagueManager.League.ILeague;
+import com.IceHockeyLeague.LeagueManager.Player.IPlayerCareerProgression;
+import com.IceHockeyLeague.LeagueManager.Player.ITeamPlayer;
+import com.IceHockeyLeague.LeagueManager.Team.ITeam;
 import com.IceHockeyLeague.StateMachine.AbstractStateMachineFactory;
 
 import java.time.LocalDate;
@@ -15,6 +21,20 @@ public class AdvanceTimeState extends AbstractState {
         league.incrementLeagueDate();
 
         LocalDate todayDate = league.getLeagueDate();
+
+        IPlayerCareerProgression playerCareerProgression = AbstractLeagueManagerFactory.getFactory().getPlayerCareerProgression();
+        for (IConference conference: league.getConferences()) {
+            for (IDivision division: conference.getDivisions()) {
+                for (ITeam team: division.getTeams()) {
+                    for (ITeamPlayer teamPlayer: team.getPlayers()) {
+                        if (teamPlayer.getInjuredStatus()) {
+                            teamPlayer.isRecovered(playerCareerProgression, todayDate);
+                        }
+                    }
+                }
+            }
+        }
+
         LocalDate regularSeasonEndDate = league.getScheduleSystem().getRegularSeasonEndDate();
         if (todayDate.isEqual(regularSeasonEndDate)) {
             nextState = AbstractStateMachineFactory.getFactory().getGeneratePlayoffScheduleState();
