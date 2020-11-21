f1665c3a53866e507cc4917a22af15f3fc04e14e
Rajveen Singh Chandhok <rj660238@dal.ca>
2020-11-20 21:03:35 -0400

    Implemented logging using log4j2

--- src/main/java/com/HockeyLeagueSimulationApp.java
+++ src/main/java/com/HockeyLeagueSimulationApp.java
@@ -3,10 +3,14 @@ package com;
 import com.IceHockeyLeague.StateMachine.IStateMachine;
 import com.IceHockeyLeague.StateMachine.IStateMachineFactory;
 import com.IceHockeyLeague.StateMachine.States.AbstractState;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class HockeyLeagueSimulationApp {
 
     public static void main(String[] args) {
+        Logger logger = LogManager.getLogger(HockeyLeagueSimulationApp.class);
+        logger.info("Starting the app");
         initializeFactories();
         runApp();
     }
