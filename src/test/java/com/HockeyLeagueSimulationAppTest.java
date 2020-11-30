d5e5dcb6290f7984448a703dd006648127ee1e11
Parth Parmar <parth.parmar@dal.ca>
2020-11-30 18:13:17 -0400

    Fixation of HockeyLeagueSimulationApp test

--- src/test/java/com/HockeyLeagueSimulationAppTest.java
+++ src/test/java/com/HockeyLeagueSimulationAppTest.java
@@ -1,5 +1,25 @@
 package com;
 
+import com.IOTest.IOMock;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class HockeyLeagueSimulationAppTest {
+    private static final String EXPECTED_FACTORY = "AppFactory";
+    private static IOMock ioMockInstance = null;
+
+    @BeforeClass
+    public static void setup() {
+        ioMockInstance = IOMock.instance();
+    }
+
+    @Test
+    public void mainFactoriesTest() {
+        ioMockInstance.commandLineInput("file.json");
+        HockeyLeagueSimulationApp.main(null);
+        String actualFactory = AbstractAppFactory.getAppFactory().getClass().getSimpleName();
+        Assert.assertEquals(EXPECTED_FACTORY, actualFactory);
+    }
 
 }
