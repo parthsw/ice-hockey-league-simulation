6df6caa18d58c752d3cea7f967053037627c645a
sagarmoghe5 <16164018+sagarmoghe5@users.noreply.github.com>
2020-11-30 21:18:29 -0400

    Added logging mechanism

--- src/main/java/com/IceHockeyLeague/Trading/GetPlayerFromTeam.java
+++ src/main/java/com/IceHockeyLeague/Trading/GetPlayerFromTeam.java
@@ -1,25 +1,19 @@
 package com.IceHockeyLeague.Trading;
 
 import com.IceHockeyLeague.LeagueManager.Player.ITeamPlayer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 
 public class GetPlayerFromTeam implements IGetPlayerFromTeam {
-    //    private IFreeAgent bestFreeAgent;
+    private final Logger LOGGER = LogManager.getLogger(GetPlayerFromTeam.class);
     private ITeamPlayer worseTeamPlayer;
 
-//    public IFreeAgent getBestAgentWithPosition(List<IFreeAgent> agents, String position) {
-//        int bestStrength = -1;
-//        for (IFreeAgent agent : agents) {
-//            if (agent.getPlayerStats().getPosition().equalsIgnoreCase(position) && bestStrength < agent.getPlayerStats().getStrength()) {
-//                this.bestFreeAgent = agent;
-//                bestStrength = (int) agent.getPlayerStats().getStrength();
-//            }
-//        }
-//        return this.bestFreeAgent;
-//    }
-
     public ITeamPlayer getWorsePlayerInTeamWithPosition(List<ITeamPlayer> players, String position) {
+        if (players.isEmpty()) {
+            LOGGER.error("Empty players provided");
+        }
         int bestStrength = Integer.MAX_VALUE;
         for (ITeamPlayer player : players) {
             if (player.getPlayerStats().getPosition().equalsIgnoreCase(position) && bestStrength > player.getPlayerStats().getStrength()) {
@@ -30,18 +24,10 @@ public class GetPlayerFromTeam implements IGetPlayerFromTeam {
         return this.worseTeamPlayer;
     }
 
-//    public IFreeAgent getRandomBestAgentSkater(List<IFreeAgent> agents){
-//        int bestStrength = -1;
-//        for (IFreeAgent agent : agents){
-//            if ((agent.getPlayerStats().getPosition().equalsIgnoreCase("Forward") || agent.getPlayerStats().getPosition().equalsIgnoreCase("Defence")) && bestStrength < agent.getPlayerStats().getStrength()) {
-//                this.bestFreeAgent = agent;
-//                bestStrength = (int) agent.getPlayerStats().getStrength();
-//            }
-//        }
-//        return this.bestFreeAgent;
-//    }
-
     public ITeamPlayer getWorseSkaterInTeam(List<ITeamPlayer> players) {
+        if (players.isEmpty()) {
+            LOGGER.error("Empty players provided");
+        }
         int bestStrength = Integer.MAX_VALUE;
         for (ITeamPlayer player : players) {
             if ((player.getPlayerStats().getPosition().equalsIgnoreCase("Forward") || player.getPlayerStats().getPosition().equals("Defence")) && bestStrength > player.getPlayerStats().getStrength()) {
